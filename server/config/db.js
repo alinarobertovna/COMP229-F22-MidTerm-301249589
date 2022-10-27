883b2b29588f73d7587ab80d9fea204b26ced0fb
Alina <alinarobertovna17.03@gmail.com>
2022-10-26 21:33:41 -0400

    create cloud database in atlas

--- server/config/db.js
+++ server/config/db.js
@@ -3,5 +3,7 @@ module.exports = {
 
   // Alina Fadeeva #301249589
   // added my local database URI
-  "URI": "mongodb://localhost/books"
+  //"URI": "mongodb://localhost/books"
+
+  "URI": "mongodb+srv://alinaf:KGh1y5Bl8Bz8UlhM@midtermcomp229.iegqeuc.mongodb.net/midtermcomp229?retryWrites=true&w=majority"
 };
